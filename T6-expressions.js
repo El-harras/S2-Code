8905d42abae5cf9af03b4257097fc5f621569dd7
Mohamed <x1man10m@gmail.com>
2026-09-22 15:47:38 +0100

    T9

--- T6-expressions.js
+++ T6-expressions.js
@@ -1,6 +1,6 @@
 //Tester plusieurs comparaisons
-console.log(100 === 100);
-console.log(10 !== 50);
+console.log(100 === "100");
+console.log(10 !== 10);
 console.log(110 > 50);
 console.log(12 < 7);
 console.log(15 >= 18);
@@ -12,13 +12,13 @@ console.log(age_1 >= 16);
 
 //Stocker le résultat
 let age_2 = 20;
-let estMajeur = age_2 >= 18;
+let estMajeur = age_2 <= 18;
 console.log(estMajeur);
 
 //Utiliser &&
 let age = 20;
 let autorisation = true;
-let acces = age >= 18 && autorisation === true;
+let acces = age >= 18 && autorisation == true;
 console.log(acces);
 
 //Utiliser ||
@@ -47,7 +47,7 @@ let Q1 = age_3 > 18;
 console.log("L’âge est-il supérieur ou égal à 18 ",Q1);
 console.log("La personne est-elle inscrite ? ",inscrit);
 console.log("Le paiement est-il effectué ? ",paiement);
-let Q4 = age_3>18 || inscrit==true;
+let Q4 = age_3>18 && inscrit==true;
 console.log("L’âge est-il supérieur ou égal à 18 ET la personne est-elle inscrite ?",Q4);
-let Q5 = inscrit==true && paiement==true;
+let Q5 = inscrit==true || paiement==true;
 console.log("La personne est-elle inscrite OU le paiement est-il effectué ? ",Q5);
--- T9-Exercices_conditions.js
+++ T9-Exercices_conditions.js
@@ -1,26 +1,3 @@
-//Exercices sur les conditions
-let age = 20;
-if(age>=18)
-    console.log("Majeur");
-else
-    console.log("Mineur");
-
-//Nombre positif ou négatif
-let Nb = 5;
-if(Nb>0)
-    console.log("Positif");
-if(Nb<0)
-    console.log("Negatif ");
-else
-    console.log("nul");
-
-//Pair ou impair
-let Nb1 = 10;
-if(Nb1%2==0)
-    console.log("Pair");
-else
-    console.log("Impair");
-
 //Résultat d’un apprenant
 let note = 14;
 if(note>=16)
@@ -30,30 +7,6 @@ else if(note<16 && note>=10)
 else
     console.log("Non validé");
 
-//Maximum de deux valeurs
-let a = 5;
-let b = 10;
-if(a>b)
-    console.log("la plus grande est ",a);
-else
-    console.log("la plus grande est ",b);
-
-//Accès à un service
-let age_1 = 22; 
-let compteActif = true;
-if(age_1>=18 && compteActif==true)
-    console.log("Acces autorise");
-else
-    console.log("Acces refuse");
-
-
-//Autorisation avec deux possibilités
-let membre = false; 
-let invitation = true;
-if(membre==false || invitation==true)
-    console.log("la personne est membre");
-else
-    console.log("la personne n'est pas membre");
 
 //Classification d’une température
 let temp = 30;
@@ -71,14 +24,3 @@ if(note_1>=10 && presence>=80)
     console.log("Validé");
 else
     console.log("Non validé");
-
-
-//Petit problème de synthèse
-let montant = 111;
-if(montant>=200)
-    console.log("Reduction de 10 % + livraison gratuite");
-if(montant<200 && montant>=100)
-    console.log("Livraison gratuite");
-else
-    console.log("Livraison payante");
-
