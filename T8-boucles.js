43063c77de13172e84d3974e81c1b44f1efb3eeb
Mohamed <x1man10m@gmail.com>
2026-09-22 16:07:25 +0100

    T8

--- T8-boucles.js
+++ T8-boucles.js
@@ -6,6 +6,12 @@ for(let i=0; i<=5; i++)
 for(let j=5; i>=0; i--)
     console.log(i);
 
+//Utiliser une condition dans une boucle
+for(let Nb=1 ;Nb<=10; Nb++){
+    if(Nb%2==0)
+        console.log(Nb);
+}
+
 // Utiliser while
 let tst_1 = 0;
 while (tst<=5){
@@ -18,30 +24,3 @@ while (tst>=0){
     console.log(i);
     tst--;
 }
-    
-//Repeter un message
-
-//Utiliser FOR
-for(let a=0 ; a<=5; a++)
-    console.log("Bonjour Mohamed" ,a);
-//Utiliser while
-let b=5;
-while(b<=0)
-    console.log("Bonjour Mohamed" ,b);
-
-//Utiliser une condition dans une boucle
-for(let Nb=1 ;Nb<=10; Nb++){
-    if(Nb%2==0)
-        console.log(Nb);
-}
-
-//Exercice guidé — Afficher une série
-for(let c=0; c<=20; c++)
-    console.log(c);
-
-//Compter les nombres pairs
-for(let d=0; d<=20; d++){
-    if(d%2==0)
-        console.log(d);
-}
-    
