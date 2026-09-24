b70e2f23723721fe848a6eb90581eaf1b9a14945
Mohamed <x1man10m@gmail.com>
2026-09-24 10:46:34 +0100

    T10

--- JavaScript code/T10-Exercices_boucles.js
+++ JavaScript code/T10-Exercices_boucles.js
@@ -1,24 +1,25 @@
 //Afficher les nombres pairs
-for (let d = 0; d <= 20; d++) {
-    if (d % 2 == 0)
-        console.log(d)
+for (let d = 1; d <= 20; d++) {
+    if (d % 2 == 0) 
+        console.log(d);
 }
 
+
 //Calculer une somme
-let som_1=0;
-for(let f=0; f<=5; f++){
-    som_1+=f;
+let som_1=1;
+for(let f=1; f<=5; f++){
+    som_1=som_1+f;
 }
 console.log("Resultat attendu : ",som_1);
 
 //Nombre de pairs et Calculer voutr somme
 let som_2 = 0;
 let cmp_pair = 0;
-for (let f = 0; f <= 20; f++) {
+for (let f = 1; f <= 20; f++) {
     if (f % 2 == 0) {
         som_2 += f;
-        cmp_pair += 1;
+        cmp_pair ++;
     }
 }
-console.log("Nombre de pairs : " + cmp_pair);
-console.log("Somme des pairs : " + som_2);
+console.log("Nombre de pairs : " , cmp_pair);
+console.log("Somme des pairs : " , som_2);
